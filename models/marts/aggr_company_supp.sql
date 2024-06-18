751473f85e4bc2128cf293dae5cf29e81f157369
unknown <saileephadtare@gmail.com>
2024-06-18 14:18:26 +0200

    company_suuplier_aggr

--- models/marts/aggr_company_supp.sql
+++ models/marts/aggr_company_supp.sql
@@ -14,19 +14,21 @@ with equip_datapoints as(
 select 
 CREATED,
 EQUIPMENT_ID,
--- ORIGIN_PID,
--- DESTINATION_PID,
+ORIGIN_PID,
+DESTINATION_PID,
 -- SUPPLIER_ID,
 -- COMPANY_ID,
 avg(USD_charges) as avg_pricing,
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY USD_charges) AS median_daily_price,
-(case when count(distinct SUPPLIER_ID)>=2 and count(distinct COMPANY_ID>=2) then True
-      when count(distinct SUPPLIER_ID)<2 and count(distinct COMPANY_ID<2) then False end) as Data_QA
+CASE 
+      WHEN COUNT(DISTINCT SUPPLIER_ID) >= 2 AND COUNT(DISTINCT COMPANY_ID) >= 5 THEN True
+      ELSE False 
+END AS dq_ok
 
 from
 equip_datapoints
 -- where created='2021-04-16' 
 -- and origin_pid='898' and destination_pid='504' 
 -- and equipment_id='2'
-group by 1,2
+group by 1,2,3,4
 order by 2
